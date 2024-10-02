c19ff2fafab6b47053c7591b66f749a9b903fbb4
Ina-Nako <inanako19@gmail.com>
2024-10-02 15:20:13 +0200

    ex 0

--- react_component/task_0/dashboard/src/Footer/Footer.js
+++ react_component/task_0/dashboard/src/Footer/Footer.js
@@ -1,16 +1,13 @@
-import logo from '../assets/holberton-logo.jpg';
-import { getFooterCopy, getFullYear } from '../utils/util';
-import './Footer.css';
 import React from 'react';
+import './Footer.css';
+import { getFooterCopy, getFullYear } from '../utils/util';
 
 function Footer() {
-  return (
-    <div className="App">Footer.css
-      <footer className="App-footer">
-        <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
-      </footer>
-    </div>
-  );
+    return (
+        <footer className="App-footer">
+            <p>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+        </footer>
+    );
 }
 
 export default Footer;
